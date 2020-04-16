e3e16bafe9542ab92acf19969ddd3d168f646079
Juan Carlos Cárdenas García <Jccarcia98@gmail.com>
2020-04-16 10:11:02 +0200

    Ejer 04

---
+++ RonaApp/Model/Usuario.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RonaApp.Model
+{
+    public class Usuario
+    {
+        public string Username { get; set; }
+        public string Pass { get; set; }
+        public Boolean PermisoAgregar { get; set; }
+        public Boolean PermisoVisualizar { get; set; }
+
+        public Usuario(string username, string pass, bool permisoAgregar, bool permisoVisualizar)
+        {
+            Username = username;
+            Pass = pass;
+            PermisoAgregar = permisoAgregar;
+            PermisoVisualizar = permisoVisualizar;
+        }
+    }
+}
