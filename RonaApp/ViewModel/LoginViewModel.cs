e9f64298493d356aa969fc55e0ad5748eda7b42b
JoseFernandezPaluzo <jose.fernandez@alumnos.fesac.es>
2020-04-21 17:48:41 +0200

    Ejercicio 3

---
+++ RonaApp/ViewModel/LoginViewModel.cs
@@ -0,0 +1,57 @@
+﻿using RonaApp.ViewModel.Base;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Input;
+
+namespace RonaApp.ViewModel
+{
+    public class LoginViewModel : BaseViewModel
+    {
+
+        //EJERCICIO 3
+        //No sabía si era necesario registrar el usuario
+        protected override void InitCommands()
+        {
+            _showMenuPageAndVerifyCommand = new RelayCommand(PerformMenu);
+        }
+
+        public LoginViewModel()
+        {
+            InitCommands();
+        }
+
+        //private string _user;
+        private string _password;
+        private RelayCommand _showMenuPageAndVerifyCommand;
+
+        /*
+        public string User
+        {
+           get => _user;
+           set => SetValue(ref _user, value);
+        }  */    
+        
+        public string Password
+        {
+            get => _password;
+            set => SetValue(ref _password, value);
+        }
+        public ICommand ShowMenuPageAndVerifyCommand => _showMenuPageAndVerifyCommand;
+
+        private void PerformMenu()
+        {
+            if (_password == "hardcodeada")
+            {
+                new MainWindow().Show();
+                Close();
+            }
+            else
+            {
+                MessageBox.Show("Porfavor ingresa una contraseña que sea correcta", "¡Alerta!", MessageBoxButton.OK);               
+            }
+            
+        }
+    }
+}
---
+++ RonaApp/Views/LoginView.xaml.cs
@@ -0,0 +1,28 @@
+﻿using RonaApp.ViewModel;
+using RonaApp.Views.Base;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace RonaApp.Views
+{
+    /// <summary>
+    /// Lógica de interacción para LoginView.xaml
+    /// </summary>
+    public partial class LoginView 
+    {
+        public LoginView()
+        {
+            InitializeComponent();
+            DataContext = new LoginViewModel();
+        }
+    }
+}
