e3f72edda54a15bcae938e7da9512f96c521aa48
JoseFernandezPaluzo <jose.fernandez@alumnos.fesac.es>
2020-04-21 18:51:04 +0200

    Ejercicio 4

---
+++ RonaApp/Model/Usuario.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RonaApp.Model
+{
+    public class Usuario
+    {
+        //Ejercicio 4
+        public string User { get; set; }
+        public string Password { get; set; }
+        public string Permisos { get; set; } //Los permisos son cadenas de texto y a la hora de verificar la contraseña, se comparará si se trata de un permiso u otro.
+    }
+}
