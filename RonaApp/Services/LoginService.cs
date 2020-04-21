8d54a219dcaf3532ec38908294e38a226f21eab4
JoseFernandezPaluzo <jose.fernandez@alumnos.fesac.es>
2020-04-22 00:32:50 +0200

    Ejercicio 5

---
+++ RonaApp/Services/LoginService.cs
@@ -0,0 +1,12 @@
+﻿using RonaApp.Model;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RonaApp.Services
+{
+    public class LoginService : ILoginService
+    {
+        public List<Usuario> Users { get; private set; } = new List<Usuario>();        
+    }
+}
--- RonaApp/ViewModel/LoginViewModel.cs
+++ RonaApp/ViewModel/LoginViewModel.cs
@@ -1,4 +1,7 @@
-﻿using RonaApp.ViewModel.Base;
+﻿using RonaApp.Model;
+using RonaApp.Services;
+using RonaApp.Services.Base;
+using RonaApp.ViewModel.Base;
 using System;
 using System.Collections.Generic;
 using System.Text;
@@ -9,9 +12,10 @@ namespace RonaApp.ViewModel
 {
     public class LoginViewModel : BaseViewModel
     {
+        private readonly ILoginService _loginService;
 
-        //EJERCICIO 3
-        //No sabía si era necesario registrar el usuario
+        //EJERCICIO 3        
+
         protected override void InitCommands()
         {
             _showMenuPageAndVerifyCommand = new RelayCommand(PerformMenu);
@@ -19,19 +23,39 @@ namespace RonaApp.ViewModel
 
         public LoginViewModel()
         {
-            InitCommands();
+            _loginService = Locator.Load<ILoginService>();
+
+            //No se si es el sitio adecuado para inicializar los usuarios.
+            //Y no se si es demasiado cutre, o estaría bien.
+            _loginService.Users.Add(new Usuario()
+            {
+                UserName = "Jose" , 
+                Password = "hardcodeada" , 
+                Permisos = "Agregar" 
+            });
+            _loginService.Users.Add(new Usuario()
+            {
+                UserName = "Juan",
+                Password = "hardcodeada",
+                Permisos = "Visualizar"
+            });
+            _loginService.Users.Add(new Usuario()
+            {
+                UserName = "Marcos",
+                Password = "hardcodeada",
+                Permisos = "Ambos"
+            });
         }
 
-        //private string _user;
+        private string _user;
         private string _password;
         private RelayCommand _showMenuPageAndVerifyCommand;
-
-        /*
+               
         public string User
         {
            get => _user;
            set => SetValue(ref _user, value);
-        }  */    
+        }     
         
         public string Password
         {
@@ -42,16 +66,39 @@ namespace RonaApp.ViewModel
 
         private void PerformMenu()
         {
-            if (_password == "hardcodeada")
+            foreach (var user in _loginService.Users)
             {
-                new MainWindow().Show();
-                Close();
+                if (_user == user.UserName)
+                {                    
+                    if (_password == "hardcodeada")
+                    {
+                        //Me falta Cambiar la vista según el tipo de permiso tenga el usuario
+                        if (user.Permisos == "Visualizar")
+                        {
+                            new MainWindow().Show();
+                            Close();
+                        }
+                        if (user.Permisos == "Agregar")
+                        {
+                            new MainWindow().Show();
+                            Close();
+                        }
+                        else
+                        {
+                            new MainWindow().Show();
+                            Close();
+                        }                        
+                    }
+                    else
+                    {
+                        MessageBox.Show("Porfavor ingresa una contraseña que sea correcta", "¡Alerta!", MessageBoxButton.OK);
+                    }
+                }
+                else
+                {
+                    MessageBox.Show("Introduce un Usuario valido");
+                }
             }
-            else
-            {
-                MessageBox.Show("Porfavor ingresa una contraseña que sea correcta", "¡Alerta!", MessageBoxButton.OK);               
-            }
-            
         }
     }
 }
